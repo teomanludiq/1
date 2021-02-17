bc632a28c9763f0cacc54223f29c01e44cb6aad2
teomanludiq <78840743+teomanludiq@users.noreply.github.com>
2021-02-17 09:42:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vuvedete dvuichno chislo: ");
+            string bin = Console.ReadLine();
+            int dec = Convert.ToInt32(bin, 2);
+            Console.WriteLine("deseti4no"+dec);
+            Console.WriteLine("dvoi4no"+bin);
+        }
+    }
+}
