1ab717865e5b12f602bfe7790c88e00d68324e98
teomanludiq <78840743+teomanludiq@users.noreply.github.com>
2021-10-10 19:23:05 +0300

    Add files via upload
    chisla

--- Program.cs
+++ Program.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 
-namespace ConsoleApp1
+namespace chisla
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Vuvedete dvuichno chislo: ");
-            string bin = Console.ReadLine();
-            int dec = Convert.ToInt32(bin, 2);
-            Console.WriteLine("deseti4no"+dec);
-            Console.WriteLine("dvoi4no"+bin);
+            string chislo1 = "1101";
+            string chislo2 = "1111";
+            int chislo5 = Convert.ToInt32(chislo1, 2);
+            int chislo6 = Convert.ToInt32(chislo2, 2);
+            int chislo3 = chislo6 + chislo5;
+            string chislo4 = Convert.ToString(chislo3, 2);
+            Console.WriteLine(chislo4);
         }
     }
 }
